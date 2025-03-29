dda4fdf829b5268f0521ffe691ebf91c14dee71c
pro100tupoi <mrpeps1@mail.ru>
2025-03-29 21:12:38 +0300

    Работа с pytest библиотекой(changes)

--- Main.py
+++ Main.py
@@ -1,21 +1,16 @@
 import requests
-# noinspection PyUnresolvedReferences
 import pytest
 
 API_key = '995fa1c5b9649503a3386aca1d3e32a4'
 city_name = 'Saint Petersburg'
 
-#http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={API_key}
-
 # Формируем URL для запроса
 Standard_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}"
 Metric_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric"
-#Imperial_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=imperial"
 
 # Отправляем GET-запрос
 responseStandard = requests.get(Standard_url)
 responseMetric = requests.get(Metric_url)
-#responseImperial = requests.get(Imperial_url)
 
 # Проверяем статус ответа
 def test_url_api_get():
@@ -27,7 +22,7 @@ data_Standard = responseStandard.json()
 
 # Извлекаем данные
 temperature_celsius = data_Metric["main"]["temp"]
-temperature_fromkelvin = data_Standard["main"]["temp"]- 273.15
+temperature_fromkelvin = data_Standard["main"]["temp"] - 273.15
 
 # Сравниваем температуру из разных API-запросов
 def test_temperature():
